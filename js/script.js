84ee1f9605d6f390c9406755353b6b8d097b4329
Wadim Martinkevich <martinkewichwadim@gmail.com>
2022-05-03 18:49:25 +0300

    feat: update burger menu

--- js/script.js
+++ js/script.js
@@ -3,6 +3,22 @@ const body = document.querySelector("body");
 const menu = document.querySelector(".menu");
 const burgerButton = document.querySelector(".burger-menu");
 const background = document.querySelector(".background");
+const menuLinks = document.querySelectorAll(".menu__link");
+
+menuLinks.forEach((menuLink)=>{
+    menuLink.addEventListener("click",()=>{
+        menu.classList.remove("active");
+        background.classList.remove("active");
+        if(burgerButton.classList.contains("active")){
+            burgerButton.classList.remove("active");
+            burgerButton.classList.add("close");
+            setTimeout(()=>{
+                burgerButton.classList.remove("close")
+            },500)
+        }
+        else burgerButton.classList.add("active");
+    })
+})
 
 colors.forEach((color)=>{
     color.addEventListener("mouseover",(e)=>{
@@ -14,9 +30,24 @@ colors.forEach((color)=>{
 burgerButton.addEventListener("click",()=>{
     menu.classList.toggle("active");
     background.classList.toggle("active");
-    burgerButton.classList.toggle("active")
+    if(burgerButton.classList.contains("active")){
+        burgerButton.classList.remove("active");
+        burgerButton.classList.add("close");
+        setTimeout(()=>{
+            burgerButton.classList.remove("close")
+        },500)
+    }
+    else burgerButton.classList.add("active");
 });
 background.addEventListener("click",()=>{
     menu.classList.remove("active");
     background.classList.remove("active");
+    if(burgerButton.classList.contains("active")){
+        burgerButton.classList.remove("active");
+        burgerButton.classList.add("close");
+        setTimeout(()=>{
+            burgerButton.classList.remove("close")
+        },500)
+    }
+    else burgerButton.classList.add("active");
 })
