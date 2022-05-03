fcc7db222d6a3800718f489fe40a1d4266bac10b
Wadim Martinkevich <martinkewichwadim@gmail.com>
2022-05-03 15:56:07 +0300

    feat: add more functions

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 const colors = document.querySelectorAll(".colors__color");
 const body = document.querySelector("body");
+const menu = document.querySelector(".menu");
+const burgerButton = document.querySelector(".burger-menu");
+const background = document.querySelector(".background");
 
 colors.forEach((color)=>{
     color.addEventListener("mouseover",(e)=>{
         let bgColor = getComputedStyle(color).backgroundColor;
         body.style.backgroundColor = bgColor;
     })
+});
+
+burgerButton.addEventListener("click",()=>{
+    menu.classList.toggle("active");
+    background.classList.toggle("active");
+});
+background.addEventListener("click",()=>{
+    menu.classList.remove("active");
+    background.classList.remove("active");
 })
